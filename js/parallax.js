07d2f3f42bcdf1348d9a7d4b2e50083b72793cf6
Jasmineee007 <3381905188@qq.com>
2026-05-19 22:50:27 +0800

    Site updated: 2026-05-19 22:50:27

--- js/parallax.js
+++ js/parallax.js
@@ -1,8 +1,10 @@
-window.addEventListener('scroll', function() {
-  var scrollY = window.scrollY;
-  var siteInfo = document.getElementById('site-info');
-  if (siteInfo && scrollY < window.innerHeight) {
-    siteInfo.style.transform = 'translateY(' + (scrollY * 0.5) + 'px)';
-    siteInfo.style.opacity = 1 - (scrollY / window.innerHeight);
-  }
-});
+var header = document.getElementById('page-header');
+if (header) {
+  window.addEventListener('scroll', function() {
+    var scrollY = window.scrollY;
+    var headerH = header.offsetHeight;
+    if (scrollY <= headerH) {
+      header.style.transform = 'translateY(' + (scrollY * 0.5) + 'px)';
+    }
+  });
+}
